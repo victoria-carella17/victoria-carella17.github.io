4d13f20e9a63db6fe84afd617683286570d9dc0e
victoria-carella17 <55209040+victoria-carella17@users.noreply.github.com>
2020-10-09 22:21:17 -0400

    Update my_javascript_code.js

--- my_javascript_code.js
+++ my_javascript_code.js
@@ -1,12 +1,12 @@
 // creating a task list in JavaScript
 
-document.addEventListener('DOMcontentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function(){
 
   document.querySelector("#new_task").onsubmit = function(){
 
     const li = document.createElement('li');
     li.innerHtml = document.querySelector('#task').value;
-    document.querySelector('#task_list').append(li);
+    document.querySelector('#tasks_list').append(li);
     document.querySelector('#task').value = '';
     return false;
 
