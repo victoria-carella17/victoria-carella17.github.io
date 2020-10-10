2b85b951aa03913fc8e23f0a8d5abef228ec08e5
victoria-carella17 <55209040+victoria-carella17@users.noreply.github.com>
2020-10-09 22:31:46 -0400

    Update my_javascript_code.js

--- my_javascript_code.js
+++ my_javascript_code.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function(){
   document.querySelector("#new_task").onsubmit = function(){
 
     const li = document.createElement('li');
-    li.innerHtml = document.querySelector('#task').value;
+    li.innerHTML = document.querySelector('#task').value;
     document.querySelector('#tasks_list').append(li);
     document.querySelector('#task').value = '';
     return false;
