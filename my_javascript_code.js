c7023cae785c88fe0de683bb1207609ce75d7887
victoria-carella17 <55209040+victoria-carella17@users.noreply.github.com>
2020-10-09 22:50:23 -0400

    Update my_javascript_code.js

--- my_javascript_code.js
+++ my_javascript_code.js
@@ -14,7 +14,13 @@ document.addEventListener('DOMContentLoaded', function(){
     document.querySelector('#tasks_list').append(li);
     document.querySelector('#task').value = '';
     return false;
-
   }
 
+  document.addEventListener('click', function(event){
+    element = event.target;
+    if(element.className === 'remove'){
+      element.parentElement.remove();
+    }
+  })
+  
 });
