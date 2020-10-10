78fad7c093b359838f47dd025d6c98b488386c8e
victoria-carella17 <55209040+victoria-carella17@users.noreply.github.com>
2020-10-09 22:39:17 -0400

    Update my_javascript_code.js

--- my_javascript_code.js
+++ my_javascript_code.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function(){
     const li = document.createElement('li');
     
     let task_text = document.querySelector('#task').value;
-    let new_task_html = ` <span> ${tast_text} </span>
+    let new_task_html = ` <span> ${task_text} </span>
                           <button class="remove"> Remove </button>`;
 
     li.innerHTML = new_task_html;
