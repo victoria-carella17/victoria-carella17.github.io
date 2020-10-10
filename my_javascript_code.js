92f085e3fdd80afc79c4b717034fce563dfa3bd2
victoria-carella17 <55209040+victoria-carella17@users.noreply.github.com>
2020-10-09 22:36:06 -0400

    Update my_javascript_code.js

--- my_javascript_code.js
+++ my_javascript_code.js
@@ -5,6 +5,13 @@ document.addEventListener('DOMContentLoaded', function(){
   document.querySelector("#new_task").onsubmit = function(){
 
     const li = document.createElement('li');
+    
+    let task_text = document.querySelector('#task').value;
+    let new_task_html = ` <span> ${tast_text} </span>
+                          <button class="remove"> Remove </button>`;
+
+    li.innerHTML = new_task_html;
+    
     li.innerHTML = document.querySelector('#task').value;
     document.querySelector('#tasks_list').append(li);
     document.querySelector('#task').value = '';
